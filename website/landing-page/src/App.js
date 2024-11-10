b46563c1d26a808ecfd41379d08ab3a5643d7869
Pranav Bedi <pranav.bedi.work@gmail.com>
2024-11-09 20:43:12 -0500

    rename and import

--- website/landing-page/src/App.js
+++ website/landing-page/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './components/NavBar';
 import HeroSection from './components/HeroSection';
 import Footer from './components/Footer';
+import ArticleShowcase from './components/ArticleShowcase';
 
 function App() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <ArticleShowcase />
       <Footer />
     </div>
   );
