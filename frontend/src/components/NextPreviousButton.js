dd9921c7f3c4424b6a78989bc75b8d802b783c58
Pranav Bedi <pranav.bedi.work@gmail.com>
2024-11-10 03:24:24 -0500

    Update NextPreviousButton.js

--- frontend/src/components/NextPreviousButton.js
+++ frontend/src/components/NextPreviousButton.js
@@ -3,13 +3,16 @@ import './NextPreviousButton.css';
 import rightArrow from '../assets/vectors/rightArrow.svg';
 import leftArrow from '../assets/vectors/leftArrow.svg';
 
-const NextPreviousButton = ({ direction }) => {
+const NextPreviousButton = ({ direction, onClick }) => {
     const isNext = direction === 'next';
     const buttonText = isNext ? 'Next' : 'Previous';
     const icon = isNext ? rightArrow : leftArrow;
 
     return (
-        <button className={`next-previous-button ${isNext ? 'next' : 'previous'}`}>
+        <button 
+            className={`next-previous-button ${isNext ? 'next' : 'previous'}`} 
+            onClick={onClick} // Added onClick prop here
+        >
             {!isNext && <img src={icon} alt="Left Arrow" className="button-icon" />}
             <span>{buttonText}</span>
             {isNext && <img src={icon} alt="Right Arrow" className="button-icon" />}
