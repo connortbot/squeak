351df3ebaf710a026bda54e5612379696bc17a6e
Saai151 <94809929+Saai151@users.noreply.github.com>
2024-11-11 11:42:39 -0500

    Basic SignUp and Login pages

---
+++ website/landing-page/src/pages/SignUp/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SignUp.css';
+
+const SignUp = () => {
+  return (
+    <div className="SignUp-container">
+      <h1>SignUp</h1>
+      {/* Your login form components */}
+    </div>
+  );
+};
+
+export default SignUp;
--- website/landing-page/src/router.js
+++ website/landing-page/src/router.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login'
+import SignUp from './pages/SignUp/SignUp'
+
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/SignUp' element={<SignUp />} />
         {/* Add more routes as needed */}
+
       </Routes>
     </BrowserRouter>
   );
